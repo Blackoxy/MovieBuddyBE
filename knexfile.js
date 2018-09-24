b61c41871f69916557ce77c0d9d8e0929968e672
Hunt Applegate <huntapplegate@gmail.com>
2018-09-24 16:10:48 -0600

    merged knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,6 @@
 
 module.exports = {
   development: {
-<<<<<<< HEAD
-<<<<<<< HEAD
     client: "pg",
     connection: "postgresql://localhost/movie_buddy"
   },
@@ -13,19 +11,3 @@ module.exports = {
     connection: process.env.DATABASE_URL
   }
 };
-=======
-    client: 'pg',
-    connection: 'postgresql://localhost/movie_buddy'
-=======
-    client: "pg",
-    connection: "postgresql://localhost/movie_buddy"
->>>>>>> c95b93b2ac8439de2b2305eecd93b0c5ae1a48c2
-
-  },
-
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL
-  }
-}
->>>>>>> 8bdcb581b768411e5370621adfad117694909465
