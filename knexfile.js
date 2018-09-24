262a0672bb0c23998aaba6392e3dd05ceaa70586
Hunt Applegate <huntapplegate@gmail.com>
2018-09-24 15:26:24 -0600

    added knex

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/movie_buddy'
+  
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL'
+    }
+  }
+}
