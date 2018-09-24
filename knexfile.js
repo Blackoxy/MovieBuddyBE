c95b93b2ac8439de2b2305eecd93b0c5ae1a48c2
Jedi77 <jprentup@hotmail.com>
2018-09-24 16:01:27 -0600

    changed knex

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'pg',
-    connection: 'postgresql://localhost/movie_buddy'
+    client: "pg",
+    connection: "postgresql://localhost/movie_buddy"
 
   },
 
   production: {
-    client: 'pg',
-    connection: 'process.env.DATABASE_URL'
+    client: "pg",
+    connection: process.env.DATABASE_URL
   }
 }
