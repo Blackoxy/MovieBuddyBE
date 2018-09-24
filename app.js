bc8c1f8a69559937b0f871f5ad6a79073724a5a5
Jedi77 <jprentup@hotmail.com>
2018-09-24 14:23:00 -0600

    added port

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+module.exports = app
+
+app.listen(port, (request, response) => {
+    console.log("Port Works!")
+})
+
+app.get("/", (request, response) => {
+    console.log("hellllo")
+})
+
