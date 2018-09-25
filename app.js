59cdbaa1955d48cdfab3a1759d2898c26154cb87
Jedi77 <jprentup@hotmail.com>
2018-09-25 10:37:06 -0600

    working post

--- app.js
+++ app.js
@@ -9,10 +9,24 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(port, (request, response) => {
-  console.log("Port Works!");
+    console.log("Port Works!");
 });
 
 app.get("/", (request, response) => {
-  //response.send("hellllo");
-  queries.getAll().then(result => response.json({ result }));
+    //response.send("hellllo");
+    queries.getAll().then(result => response.json({ result }));
 });
+
+app.get("/:id", (request, response) => {
+    queries.getBuddyById(request.params.id).then(result => response.json({ result }))
+})
+
+app.post("/", (request, response) => {
+    queries.createBuddy(request.body).then(result => response.json({ result }))
+})
+app.put("/:id", (request, response) => {
+    queries.updateBuddy(request.params.id, request.body).then(result => response.json({ result }))
+})
+app.delete("/:id", (request, response) => {
+    queries.deleteBuddy(request.params.id).then(result => response.json({ result }))
+})
--- queries.js
+++ queries.js
@@ -2,13 +2,23 @@ const connection = require("./knexfile")[process.env.NODE_ENV || "development"];
 const knex = require("knex")(connection);
 
 module.exports = {
-  getAll() {
-    return knex.select().from("movie_buds");
-  },
-  getBuddyById(id) {
-    return knex
-      .select()
-      .from("movie_buds")
-      .where("id", id);
-  }
+    getAll() {
+        return knex.select().from("movie_buds");
+    },
+    getBuddyById(id) {
+        return knex
+            .select()
+            .from("movie_buds")
+            .where("id", id);
+    },
+    createBuddy(buddy) {
+        return knex("movie_buds").insert(buddy, ["id", "firstName", "lastName", "userId"])
+
+    },
+    updateBuddy(id, body) {
+        return knex.select().from("movie_buds").where("id", id).update(body).returning("*")
+    },
+    deleteBuddy(id) {
+        return knex.select().from("movie_buds").where("id", id).del().returning("*")
+    }
 };
