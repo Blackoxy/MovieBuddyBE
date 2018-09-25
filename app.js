eb53933f08f99703857f6f8b246f6267fa7faba5
Anh <blackoxy@gmail.com>
2018-09-25 14:57:45 -0600

    fixed error handling

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const queries = require("./queries");
+const morgan = require('morgan')
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(port, (request, response) => {
-  console.log("Port Works!");
+  console.log(`listening on ${port}`);
 });
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
   //response.send("hellllo");
   queries.getAll().then(result => response.json({ result }));
 });
+
+app.get('/*', function(req, res, next) {
+  res.json({message:'Invalid URL'})
+});
+
+app.use(function(error, req, res, next) {
+  // Won't get here, because Express doesn't catch the above error
+  res.json({ message: error.message });
+});
+
+
+
