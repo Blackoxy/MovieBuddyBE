5511d7828c8325cddb8e9422619e3413f17b383f
Hunt Applegate <huntapplegate@gmail.com>
2018-09-26 12:48:11 -0600

    stashing

--- migrations/20180925122129_joins.js
+++ migrations/20180925122129_joins.js
@@ -2,7 +2,7 @@
 exports.up = function (knex, Promise) {
 
     return knex.schema.createTable('buddies_titles', buddy_titles => {
-        buddy_titles.increments('id')
+        buddy_titles.increments('id').primary()
         buddy_titles.integer('titles_info').unsigned()
         buddy_titles.foreign('titles_info').references('movie_titles.id')
         buddy_titles.integer('buds_info').unsigned()
